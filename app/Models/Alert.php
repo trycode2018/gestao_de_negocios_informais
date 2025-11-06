5fd242732ea2f8bfe5d327879b4e6ece5fe6d0d4
Henrique Ventura <henriqueventura2022@gmail.com>
2025-11-06 21:20:13 +0000

    retificando erro

--- app/Models/Alert.php
+++ app/Models/Alert.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
-{
+{ 
     // Modelo de Alerta 
 
     use HasFactory;
 
+    
     protected $fillable = ['user_id', 'type', 'message', 'is_read'];
 
     public function user()
